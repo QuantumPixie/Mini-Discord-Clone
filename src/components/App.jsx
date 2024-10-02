1c744503e3ce0f596092c64b404313877b6a0a4f
Tatjana Huppertz <tatjana.huppertz@gmail.com>
2024-10-02 11:49:46 +0100

    changed chat header color to white for better visibility

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,6 @@ import ChatArea from './Chatarea/ChatArea'
 import './App.css'
 
 const App = () => {
-  console.log('render')
   const [socket, setSocket] = useState(null)
   const [user, setUser] = useState(null)
   const [channels, setChannels] = useState([])
